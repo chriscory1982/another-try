4dde2b4024b1a0ea7a397bc7b1c2e8d72563451f
chriscory1982 <97462622+chriscory1982@users.noreply.github.com>
2022-01-11 15:30:47 -0500

    Initial commit

---
+++ BankingDomain/BankAccount.cs
@@ -0,0 +1,29 @@
+﻿namespace BankingDomain
+{
+    public class BankAccount
+    {
+        private decimal _balance = 5000;
+       
+        public decimal GetBalance()
+        {
+            return _balance;
+        }
+
+        public void Deposit(decimal amountToDeposit)
+        {
+            _balance += amountToDeposit;
+        }
+
+        public void Withdraw(decimal amountToWithdraw)
+        {
+            // "Guard" - don't do any work unless these preconditions are met.
+            if(amountToWithdraw > _balance)
+            {
+                // Abnormally End.
+                throw new OverdraftException();
+            }
+
+            _balance -= amountToWithdraw;
+        }
+    }
+}
